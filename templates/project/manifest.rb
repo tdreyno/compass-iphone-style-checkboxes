eadd319fbaa7bd6e9c7aadc924212d6fd9de048c
Thomas Reynolds <tdreyno@gmail.com>
2010-08-17 15:50:55 -0700

    vendor js

--- templates/project/manifest.rb
+++ templates/project/manifest.rb
@@ -1,5 +1,6 @@
 # Make sure you list all the project template files here in the manifest.
 stylesheet 'iphone-style-example.sass', :media => 'screen, projection'
+javascript 'iphone-style-checkboxes.js'
 image 'iphone-style-checkboxes/off.png'
 image 'iphone-style-checkboxes/on.png'
 image 'iphone-style-checkboxes/slider.png'
